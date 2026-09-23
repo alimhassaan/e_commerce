93d100988d48f055f99ae8ce129867bf4528bcb6
Ali Mohamed Hassaan <alimhassaan442007@gmail.com>
2026-09-24 02:43:58 +0300

    Implement Firebase authentication and update package identifiers

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:e_commerce/utilities/app_router.dart';
 import 'package:e_commerce/utilities/app_routes.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
